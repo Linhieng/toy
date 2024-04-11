905902baa1552b967c3d5f62292b2972489be70d
linhieng <790728740@qq.com>
2024-04-11 22:41:01 +0800

    update AutoTask powershell script path

--- AutoTask/scripts/scheduled-task.ps1
+++ AutoTask/scripts/scheduled-task.ps1
@@ -11,9 +11,9 @@ if ($taskExists) {
 }
 
 # 如果需要隐藏窗口，可以直接在 Start-Process 后添加 -WindowStyle hidden
-$action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "-Command `"Start-Process -FilePath pwsh.exe -ArgumentList 'D:\github-code\AutoTask\scripts\auto-push.ps1'`""
+$action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "-Command `"Start-Process -FilePath pwsh.exe -ArgumentList 'D:\.scripts\auto-push.ps1'`""
 # 每晚 10:20 分
-$trigger = New-ScheduledTaskTrigger -Daily -At "22:20:00 PM"
+$trigger = New-ScheduledTaskTrigger -Daily -At "22:36:00 PM"
 # 需要有网络时才运行
 $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
 $description = "这是一个定时推送本地 git 仓库的计划任务"
